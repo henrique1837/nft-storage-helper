67cb3c5deaaf2921f72a2bed02ca9840cdebf62a
henrique <bongador@protonmail.com>
2022-07-12 13:38:24 -0500

    Using thegraph to get NFTs, tested polygon and xdai

--- index.js
+++ index.js
@@ -1,35 +1,41 @@
 import ethers from 'ethers';
 import getAndPinFromPublicGateways from './utils/getAndPinFromPublicGateways.js';
 import checkDirectories from './utils/checkDirectories.js';
-
-// ERC1155 URI event https://docs.openzeppelin.com/contracts/4.x/api/token/erc1155#IERC1155-URI-string-uint256- //
-const abi = [{"type":"event","name":"URI","inputs":[{"type":"string","name":"value","internalType":"string","indexed":false},{"type":"uint256","name":"id","internalType":"uint256","indexed":true}],"anonymous":false}]
+import {initiateClient,getERC1155From,getERC721From} from './utils/graphs.js';
 
 const provider = new ethers.providers.JsonRpcProvider(process.env.RPC);
-const contract = new ethers.Contract(process.env.ADDRESS,abi,provider);
 const fromId = process.env.FROM_ID;
+const type = process.env.TYPE
 
 async function main(){
 
   checkDirectories();
 
   console.log('Getting all URIs from NFT contract');
-  let filter = contract.filters.URI(null,null);
-  let results = await contract.queryFilter(filter,0,'latest');
-  if(fromId){
-    console.log(`Filtering from id ${fromId}`);
-    results = results.filter(res => {
-      return(Number(res.args.id) >= Number(fromId));
-    })
+  const client = await initiateClient(provider);
+  let nftsGraph;
+  let results;
+  if(type?.toLowerCase() == "erc721"){
+    console.log("ERC721 contract");
+    nftsGraph = await getERC721From(client);
+    results = nftsGraph.erc721Tokens;
+  } else {
+    console.log("ERC1155 contract");
+    nftsGraph = await getERC1155From(client);
+    results = nftsGraph.erc1155Tokens;
+
   }
-  console.log(`Total of ${results.length} to be pinned`);
+  if(results.length == 0){
+    console.log("Could not get NFTs, check if FROM_ID and TYPE are correct");
+    process.exit();
+  }
   console.log('Getting cids from metadata and images, pin and save local');
   let i = 1;
 
   for(let res of results){
 
-    let cid = res.args.value;
-    let id = res.args.id;
+    let cid = res.uri;
+    let id = res.identifier;
     console.log(`Getting data from token id ${id}, cid: ${cid}`);
     const savedObj = await getAndPinFromPublicGateways(cid);
     console.log(savedObj);
